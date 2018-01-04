0cf0e72c910362ddeca35324597e12a370c88be4
Rohit Parab <rohit@Rohits-MacBook-Air.local>
2018-01-04 10:52:20 +0530

    Comit changes

--- main.go
+++ main.go
@@ -1,83 +1,33 @@
 package main
 
 import (
-	"crypto/sha1"
 	"fmt"
-	"html/template"
 	"io"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
-}
-
 func main() {
-	http.HandleFunc("/", index)
-	http.Handle("/favicon.ico", http.NotFoundHandler())
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", upload)
+
 }
-
-func index(w http.ResponseWriter, req *http.Request) {
-	//c := getCookie(w, req)
-	// process form submission
-	if req.Method == http.MethodPost {
-		mf, fh, err := req.FormFile("nf")
+func upload(wr http.ResponseWriter, rq *http.Request) {
+	fmt.Println(rq.Method)
+	if rq.Method == "POST" {
+		rq.ParseMultipartForm(32 << 20)
+		file, fileHdr, err := rq.FormFile("nf")
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
-		defer mf.Close()
-		// create sha for file name
-		ext := strings.Split(fh.Filename, ".")[1]
-		h := sha1.New()
-		io.Copy(h, mf)
-		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
-		// create new file
-		wd, err := os.Getwd()
+		defer file.Close()
+		fmt.Fprintf(wr, "%v", fileHdr)
+		f, err := os.OpenFile("./test/"+fileHdr.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
-		path := filepath.Join(wd, "public", "pics", fname)
-		nf, err := os.Create(path)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer nf.Close()
-		// copy
-		mf.Seek(0, 0)
-		io.Copy(nf, mf)
-		// add filename to this user's cookie
-		//c = appendValue(w, c, fname)
+		defer f.Close()
+		io.Copy(f, file)
 	}
-	xs := "Hello" //strings.Split(c.Value, "|")
-	tpl.ExecuteTemplate(w, "index.gohtml", xs)
 }
-
-/* func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
-	c, err := req.Cookie("session")
-	if err != nil {
-		sID := uuid.NewV4()
-		c = &http.Cookie{
-			Name:  "session",
-			Value: sID.String(),
-		}
-		http.SetCookie(w, c)
-	}
-	return c
-} */
-
-// takes in a file name now also
-/* func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
-	s := c.Value
-	if !strings.Contains(s, fname) {
-		s += "|" + fname
-	}
-	c.Value = s
-	http.SetCookie(w, c)
-	return c
-} */
